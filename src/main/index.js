7d2eb8557de5fd5524cdaafd5b6faf1f83afb4b4
h-shan <howardshan11@gmail.com>
2017-10-30 17:00:54 -0500

    Add menu to allow copy and paste

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,8 @@
 'use strict';
 
-import { app, BrowserWindow } from 'electron';
+import { app, Menu, BrowserWindow } from 'electron';
 import Store from 'electron-store';
+
 const store = new Store({
   name: 'user-prefs',
   defaults: {
@@ -49,6 +50,27 @@ function createWindow() {
     // Now that we have them, save them using the `set` method.
     store.set('windowBounds', { width, height });
   });
+
+  var template = [{
+    label: 'Application',
+    submenu: [
+      { label: 'About Application', selector: 'orderFrontStandardAboutPanel:' },
+      { type: 'separator' },
+      { label: 'Quit', accelerator: 'Command+Q', click: function() { app.quit(); } }
+    ]}, {
+    label: 'Edit',
+    submenu: [
+      { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+      { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+      { type: 'separator' },
+      { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+      { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+      { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+      { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
+    ]}
+  ];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
 
 app.on('ready', createWindow);
